b986c590526c22dc8cbb2854e3e934a5929b136a
xrq <15210840689@163.com>
2019-12-07 18:04:57 +0800

    增加URL参数获取

---
+++ utils/tool.js
@@ -0,0 +1,15 @@
+const tool = {
+    /**
+     * 获取URL参数
+     * @param {String} variable 要查询的属性值
+     */
+    getQueryVariable: function (variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) { return pair[1]; }
+        }
+        return (false);
+    }
+}
